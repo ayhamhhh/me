0e49232bd8164d58f95ab879466576d7c442aaa9
ayham <ayhamhhh00@gmail.com>
2022-03-23 19:49:55 +0200

    mm

--- react-portfolio/src/components/experience/experience.jsx
+++ react-portfolio/src/components/experience/experience.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Experience.css'
-import {BsPatchCheck} from 'react-icons/bs'
+import { BsPatchCheck } from 'react-icons/bs'
 
 const Experience = () => {
   return (
@@ -13,34 +13,82 @@ const Experience = () => {
           <h3>frontend development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheck />
-              <h4>HTML</h4>
-              <small className='text-light'>Experience</small>
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experience</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheck />
-              <h4>css</h4>
-              <small className='text-light'>Experience</small>
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+                <h4>css</h4>
+                <small className='text-light'>Experience</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheck />
-              <h4>javascript</h4>
-              <small className='text-light'>Experience</small>
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+                <h4>bootstarp</h4>
+                <small className='text-light'>Experience</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheck />
-              <h4>bootstrap</h4>
-              <small className='text-light'>Experience</small>
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+                <h4>mmm</h4>
+                <small className='text-light'>Experience</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsPatchCheck />
-              <h4>tawilwind</h4>
-              <small className='text-light'>Experience</small>
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+                <h4>among</h4>
+                <small className='text-light'>Experience</small>
+              </div>
             </article>
           </div>
         </div>
+        {/* end of frontend */}
         <div className="experience__backend">
-
+          <h3>backend development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+                <h4>nodo</h4>
+                <small className='text-light'>mongo</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+                <h4>nodo</h4>
+                <small className='text-light'>mongo</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheck />
+              <div>
+                <h4>nodo</h4>
+                <small className='text-light'>mongo</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+                <h4>nodo</h4>
+                <small className='text-light'>mongo</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheck className='experience__details-icon' />
+              <div>
+                <h4>nodo</h4>
+                <small className='text-light'>mongo</small>
+              </div>
+            </article>
+          </div>
         </div>
       </div>
     </section>
